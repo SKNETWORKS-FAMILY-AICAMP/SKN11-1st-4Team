ab027e0613e71a589fea6b92fc7c111f63e055c0
SangJoonLee <haides1998@gmail.com>
2025-02-26 11:16:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,53 @@
 import mysql.connect as mysql
 import pandas as pd
 
-# run database script
-connection = mysql.connect(
-  host = 'localhost',
-  user = 'root',
-  password = '', # 실행하는 pc에 따라
-  database = 'mysql'
-)
-cursor = connection.cursor()
+def run_db_script():
+  # run database script and insert initial values
+  connection = mysql.connect(
+    host = 'localhost',
+    user = 'root',
+    password = '', # 실행하는 pc에 따라
+    database = 'mysql'
+  )
+  cursor = connection.cursor()
+  
+  with open('./green_car.sql', 'r') as f:
+    sqlFile = f.read()
+  f.close()
+  
+  commands = sqlFile.split(';')
+  for command in commands:
+    try:
+      if command.strip() != '':
+        cursor.execute(command)
+    except Exception e:
+      print(f'Command skipped: {command}')
+      print(e)
 
-with open('./green_car.sql', 'r') as f:
-  sqlFile = f.read()
-f.close()
-
-commands = sqlFile.split(';')
-for command in commands:
-  try:
-    if command.strip() != '':
-      cursor.execute(command)
-  except Exception e:
-    print(f'Command skipped: {command}')
-    print(e)
-
-
-# insert region values
-with open('./data/eco_car_registration.csv', 'r') as f:
-  df = pd.read_csv(f)
-f.close()
-
-query = 'INSERT INTO region VALUES (%s, %s)'
-i = 1
-for region in df.iloc[2:19, :1].iloc[:, 0]:  # bad
-  cursor.execute(query, (i, region))
-  i+=1
-
-connection.commit()
+  # insert initial values into region table
+  with open('./data/eco_car_registration.csv', 'r') as f:
+    df = pd.read_csv(f)
+  f.close()
+  
+  query = 'INSERT INTO region VALUES (%s, %s)'
+  i = 1
+  for region in df.iloc[2:19, :1].iloc[:, 0]:  # bad
+    cursor.execute(query, (i, region))
+    i+=1
+  
+  connection.commit()
+  return
 
 
-# load databases
+def load_databases():
+  return
 
-# render streamlit
+def render_streamlit():
+  return
+
+if __name__ == "__main__":
+  run_db_script()
+  load_databases()
+  render_streamlit()
+
+  exit()
