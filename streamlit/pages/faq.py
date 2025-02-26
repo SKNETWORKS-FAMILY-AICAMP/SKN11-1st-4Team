0bb8124ec5f0f95ab59ba70c62464b3c0b21d88e
SangJoonLee <haides1998@gmail.com>
2025-02-26 12:15:41 +0900

    Update faq.py

--- streamlit/pages/faq.py
+++ streamlit/pages/faq.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-faq_abspath = os.path.abspath('/data/FAQ')
+
 
 # CSV 파일 불러오기
 def load_data(file_paths):
@@ -16,7 +16,7 @@ def run():
     
     # 데이터 로드
     file_paths = ["ev_or_faq_data.csv", "kia_faq_data.csv", "chevrolet_faq_data.csv"]  # CSV 파일 경로 리스트
-    file_paths = [os.path.join(faq_abspath, file_path) for file_path in file_paths] 
+    file_paths = [os.path.join(os.path.relpath('../data/FAQ', file_path) for file_path in file_paths] 
     try:
         df = load_data(file_paths)
         st.success("FAQ 데이터 업데이트 완료!")
