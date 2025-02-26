0b5013292490636f54ece0de59131cf3693fa65a
SangJoonLee <haides1998@gmail.com>
2025-02-26 10:40:19 +0900

    Update green_car.sql

--- green_car.sql
+++ green_car.sql
@@ -14,11 +14,10 @@ CREATE TABLE charger_type (
 )ENGINE=INNODB;
 
 CREATE TABLE region(
-    region_key INT PRIMARY KEY AUTO_INCREMENT,
+    region_key INT PRIMARY KEY AUTO_INCREMENT, -- files 폴더에 있는 eco_registration에 있는 서울 부산 대구... 제주 순으로 region_key가 1부터 17까지 부여됨
     region_name VARCHAR(255) NOT NULL
 )ENGINE=INNODB;
 
--- files 폴더에 있는 eco_registration에 있는 서울 부산 대구... 제주 순으로 region_key가 1부터 17까지 부여됨
 
 CREATE TABLE regional_charger_count(
     region_key INT,
@@ -30,13 +29,10 @@ CREATE TABLE regional_charger_count(
     FOREIGN KEY(charger_key) REFERENCES charger_type(charger_key)
 )ENGINE=INNODB;
 
-CREATE TABLE regional_entire_car_count(
-    -- TODO; 연료별 자동차 긁어오기
-)
 
-CREATE TABLE regional_eco_car_count(
+CREATE TABLE regional_car_count(
     region_key INT,
-    fuel_type VARCHAR(255) NOT NULL,
+    fuel_type VARCHAR(255) NOT NULL, -- elec, hydro, others
     car_count INT,
     car_count_date DATE,
     PRIMARY KEY(region_key, fuel_type, car_count_date),
