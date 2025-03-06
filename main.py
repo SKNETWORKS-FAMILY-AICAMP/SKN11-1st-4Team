3acb79e97d10b9f2bc4b038f502eb63e36c5b76a
SangJoonLee <haides1998@gmail.com>
2025-03-06 15:19:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,89 +1,92 @@
-import mysql.connector as mysql
+import argparse
+import streamlit.web.cli as cli
 import pandas as pd
-import re, sys
-from streamlit import cli as strcli
+import mysql.connector as mysql
+from string import Template
 
-def run_db_script(connection):
-  """
-  DB 초기 설정이 담긴 script (green_car.sql)를 실행시킴.
-  """
-  
-  cursor = connection.cursor()
-  
-  with open('./green_car.sql', 'r') as f:
-    sqlFile = f.read()
-  f.close()
-  
-  commands = sqlFile.split(';')
-  for command in commands:
-    try:
-      if command.strip() != '':
-        cursor.execute(command)
-    except Exception as e:
-      print(f'Command skipped: {command}')
-      print(e)
+def run_sql_script(connection, user_database_name):
+    cursor = connection.cursor()
 
-  # insert initial values into region table
-  with open('./data/eco_car_registration.csv', 'r') as f:
-    df = pd.read_csv(f)
-  f.close()
-  
-  query = 'INSERT INTO region VALUES (%s, %s)'
-  i = 1
-  for region in df.iloc[2:19, :1].iloc[:, 0]:  # bad implementation; need extra preprocessing method to wrap this 
-    cursor.execute(query, (i, region))
-    i+=1
-  
-  connection.commit()
-  cursor.close()
-  return
+    with open('./eco_car.sql', 'r') as f:
+        sql = f.read()
+
+    commands = sql.split(';')
+    for command in commands:
+        try:
+            if 'CREATE DATABASE' in command or 'USE' in command: # replace database name
+                command = Template(sql).substitute(
+                    database_name = user_database_name
+                )
+            if command.strip() != '':
+                cursor.execute(command)
+        except Exception as e:
+            print("Command skipped: ", command)
+            print(e)
+
+    connection.commit()
+    cursor.close()
 
 
 def load_data_to_db(connection):
-  """
-  데이터들을 DB에 로드함.
-  """
-  from eco_car_DB import populate_region_table, process_csv_and_insert
 
-  # 친환경자동차 data load
-  
-  eco_car_csv = './data/eco_car_registration.csv'
-  populate_region_table(connection)
-  process_csv_and_insert(eco_car_csv, connection)
-  
-  # TODO
-  # 전체 자동차 등록 현황 data load
+    cursor = connection.cursor()
 
-  # TODO
-  # 충전소 현황 data load
-  
-  return
-
-def render_streamlit():
-  """
-  streamlit을 실행함 - terminal이 아니라 python 내부에서 실행
-  """
-  
-  sys.argv = ['streamlit', 'run', './streamlit/main_page.py']
-  sys.exit(strcli.main())
-  return
+    # TODO: load entire car registration data to MYSQL
 
 
+    # load eco car registration data to MYSQL
+    from eco_car_DB import populate_region_table, process_csv_and_insert
 
-def main():
-  connection = mysql.connect(
-    host = 'localhost',
-    user = 'root',
-    password = '', # 실행하는 pc에 따라
-    database = 'mysql'
-  )
+    eco_car_csv = pd.read_csv('/data/eco_car_registration.csv')
+    populate_region_table(connection)
+    process_csv_and_insert(eco_car_csv, connection)
 
-  run_db_script(connection)
-  load_data_to_db(connection)
-  render_streamlit_page()
+    # TODO: load eco car charging station data to MYSQL
 
-  connection.close()
-  
+    # TODO: load FAQ data to MYSQL
+
+    connection.commit()
+    cursor.close()
+
+
+def main(args):
+    # connect to MySQL
+    init_connection = mysql.connect(
+        host=args.host,
+        user=args.user,
+        password=args.password,
+        database='mysql'
+    )
+
+    # create database using sql script
+    run_sql_script(init_connection, args.database_name)
+
+    # connect to created database
+    connection = mysql.connect(
+        host=args.host,
+        user=args.user,
+        password=args.password,
+        database=args.database_name
+    )
+
+    # load datas to created database
+    load_data_to_db(connection)
+    connection.close()
+
+    # render pages using streamlit
+    cli.main_run(['./streamlit/main_page.py', '--server.port', str(args.streamlit_port)])
+    return
+
 
 if __name__ == "__main__":
-  main()
+    # argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", type=str, default="localhost", help="MySQL host")
+    parser.add_argument("--user", type=str, default="root", help="Username for MySQL host")
+    parser.add_argument("--password", type=str, default="", help="Password for MySQL host")
+    parser.add_argument("--database_name", type=str, default="eco_car", \
+                        help="Database name that you want to use for this project")
+    parser.add_argument("--streamlit_port", type=int, default=8501, help="Port number for Streamlit host")
+    
+    args = parser.parse_args()
+    main(args)
